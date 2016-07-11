9354569ffd91a18955cc0c020e28dba8516d54ac
Ryan O'Hern <ryanohern@OHerns-MacBook-Air.local>
2016-07-11 17:28:49 -0400

    moved server.js back to it's own file

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
-var http = require('http');
+// var http = require('http');
 
 
-function onRequest(request, response){
-	console.log("Our user made a request" + request.URL); //log something out
-	response.writeHead(200, {"Context-Type": "text/plain"}); //set up response and send text
-	response.write("Here is your crazy data, bro."); //send the text or info
-	response.end(); //
-}
+// function onRequest(request, response){
+// 	console.log("Our user made a request" + request.URL); //log something out
+// 	response.writeHead(200, {"Context-Type": "text/plain"}); //set up response and send text
+// 	response.write("Here is your crazy data, bro."); //send the text or info
+// 	response.end(); //
+// }
 
-http.createServer(onRequest).listen(8888);
-console.log("Server is now running");
+// http.createServer(onRequest).listen(8888);
+// console.log("Server is now running");
 
 $( document ).ready(function(){
 
