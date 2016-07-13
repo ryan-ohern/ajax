b14e0d91783d90414c6d3cbba550486d769b62e1
Ryan O'Hern <ryanohern@gmail.com>
2016-07-13 13:08:52 -0400

    updated server.js to also listen to local host

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/weather.html');
 });
 
-http.listen(process.env.PORT || 3000, function(){
+http.listen(process.env.PORT && 3000, function(){
 	console.log('listening on *:3000');
 });
 
