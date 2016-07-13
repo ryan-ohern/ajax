137170914475bbcf77c14a505f0c0e0ecf8aa28f
Ryan O'Hern <ryanohern@gmail.com>
2016-07-13 13:10:13 -0400

    reverted server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.use('/static', express.static(__dirname + '/public'));
+// app.use('/static', express.static(__dirname + '/public'));
+// app.use(express.static('public'));
 // app.use(express.static('src/views'));
+
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/weather.html');
 });
 
-http.listen(process.env.PORT && 3000, function(){
+http.listen(process.env.PORT || 3000, function(){
 	console.log('listening on *:3000');
 });
 
