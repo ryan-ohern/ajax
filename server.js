b26adcf875c845e2e0525d4265ec155d765bd5dd
Ryan O'Hern <ryanohern@gmail.com>
2016-07-13 15:58:43 -0400

    test

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
-var app = require('express')();
-var http = require('http').Server(app);
+// var app = require('express')();
+// var http = require('http').Server(app);
 
-// app.use('/static', express.static(__dirname + '/public'));
-// app.use(express.static('public'));
-// app.use(express.static(__dirname + '/public'));
-// app.use(express.static('src/views'));
-app.use(express.static(path.join(__dirname, '/public')));
+// // app.use('/static', express.static(__dirname + '/public'));
+// // app.use(express.static('public'));
+// // app.use(express.static(__dirname + '/public'));
+// // app.use(express.static('src/views'));
+// app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/weather.html');
+// app.get('/', function(req, res){
+// 	res.sendFile(__dirname + '/weather.html');
+// });
+
+// http.listen(process.env.PORT || 3000, function(){
+// 	console.log('listening on *:3000');
+// });
+
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('weather');
 });
 
-http.listen(process.env.PORT || 3000, function(){
-	console.log('listening on *:3000');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
-
