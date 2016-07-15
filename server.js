4ba01505783edb9ad99beb1646a3bbc72f780258
Ryan O'Hern <ryanohern@gmail.com>
2016-07-15 15:39:27 -0400

    test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
 // app.use(express.static(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname + './public')));
 // app.use(express.static('./public'));
 
 // set the home page route
